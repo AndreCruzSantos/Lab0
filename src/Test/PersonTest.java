430050197d1ff791a95507e1e87e7c224b41fda8
AndreCruzSantos <72869760+AndreCruzSantos@users.noreply.github.com>
2020-10-14 14:59:08 +0100

    new test class

--- src/Test/PersonTest.java
+++ src/Test/PersonTest.java
@@ -1,7 +1,14 @@
 package Test;
 
-public class PersonTest {
-    public void testGetName(){
+import com.pa.Person;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+public class PersonTest {
+
+    @Test
+    public void testGetId(){
+        Person p = new Person(1,"Andre");
+        assertEquals(p.getId(),1);
     }
 }
